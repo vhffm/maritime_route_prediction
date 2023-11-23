f29aab74754a213daf6d32b393f7e2a8b2a6b767
janhendrikw <jandrik91@googlemail.com>
2023-11-23 09:06:10 +0100

    refactor of class MOGenPathPrediction. moved plotting function into visualization utils.

--- src/models/.ipynb_checkpoints/MOGen_path_prediction-checkpoint.py
+++ src/models/.ipynb_checkpoints/MOGen_path_prediction-checkpoint.py
@@ -13,7 +13,6 @@ import time
 import warnings
 import pickle
 import sys
-from io import StringIO
 
 # add paths for modules
 sys.path.append('../features')
@@ -251,74 +250,6 @@ class MOGenPathPrediction:
         SSPDs = np.array(SSPDs)
         print(f'Mean SSPD: {np.mean(SSPDs)}m ({np.mean(SSPDs[SSPDs>0])}m)')
                               
-
-    def plot_predictions(self, predictions, start_node, trajectory, true_path, network, end_node=None, min_passages=3, location='stavanger', opacity=0.2):
-        '''
-        Plots predictions on an interactive map
-        ====================================
-        Params:
-        predictions: output of self.predict_next_node function
-        start_node: Single start node or start node sequence underlying the prediction
-        trajectory: original trajectory that we want to make predictions for
-        true_path: the actual path in the graph belonging to the original trajectory
-        network: the underlying MaritimeTrafficNetwork object
-        min_passages: only edges are plotted that have at least min_passages as edge feature
-        center: center point of the plotted map
-        opacity: opacity of the waypoints and edges of the maritime traffic network
-        ====================================
-        Returns:
-        map: folium map object to display
-        '''
-        # plot network and basemap
-        map = network.map_graph(pruned=True, min_passages=min_passages, 
-                                location=location, opacity=opacity)
-
-        # highlight the start node(s)
-        wps = network.waypoints[network.waypoints.clusterID.isin(start_node)]
-        columns_points = ['clusterID', 'geometry', 'speed', 'cog_before', 'cog_after', 'n_members']  # columns to plot
-        columns_hull = ['clusterID', 'convex_hull', 'speed', 'cog_before', 'cog_after', 'n_members']  # columns to plot
-        wps.set_geometry('geometry', inplace=True)
-        map = wps[columns_points].explore(m=map, name='start nodes', legend=False,
-                                          marker_kwds={'radius':3},
-                                          style_kwds={'color':'yellow', 'fillColor':'yellow', 'fillOpacity':1, 'opacity':1})
-        wps.set_geometry('convex_hull', inplace=True, crs=network.crs)
-        map = wps[columns_hull].explore(m=map, name='start nodes convex hulls', legend=False,
-                                        style_kwds={'color':'yellow', 'fillColor':'yellow', 'fillOpacity':0.3, 'opacity':1})
-
-        if end_node is not None:
-            wps = network.waypoints[network.waypoints.clusterID==end_node]
-            wps.set_geometry('geometry', inplace=True)
-            map = wps[columns_points].explore(m=map, name='end node', legend=False,
-                                              marker_kwds={'radius':3},
-                                              style_kwds={'color':'orange', 'fillColor':'orange', 'fillOpacity':1, 'opacity':1})
-            wps.set_geometry('convex_hull', inplace=True, crs=network.crs)
-            map = wps[columns_hull].explore(m=map, name='end_node convex hull', legend=False,
-                                            style_kwds={'color':'orange', 'fillColor':'orange', 'fillOpacity':0.3, 'opacity':1})
-            
-        
-        # generate plottable dataframe of predicted paths
-        predicted_paths = pd.DataFrame(columns=['path', 'geometry', 'probability'])
-        for key, value in predictions.items():
-            path = start_node + [x for x in key]
-            line = geometry_utils.node_sequence_to_linestring(path, network.waypoint_connections)
-            temp = pd.DataFrame([[tuple(path), line, value]], columns=['path', 'geometry', 'probability'])
-            predicted_paths = pd.concat([predicted_paths, temp])
-        predicted_paths = gpd.GeoDataFrame(predicted_paths, geometry='geometry', crs=network.crs)
-        
-        # plot prediction and ground truth
-        true_path_line = geometry_utils.get_geo_df(true_path, network.waypoint_connections)
-        trajectory = trajectory.to_line_gdf()
-        mmsi = trajectory['mmsi'].unique()
-
-        map = trajectory[['mmsi', 'geometry', 'skipsgruppe', 'length', 'bredde']].explore(m=map, style_kwds={'weight':3, 'color':'black', 'opacity':1},
-                                                                                          name=f'{mmsi} trajectory')
-        map = true_path_line.explore(m=map, style_kwds={'weight':3, 'color':'cyan', 'opacity':1},
-                                   name=f'{mmsi} closest path')
-        for i in range (0, len(predicted_paths)):
-            map = predicted_paths.iloc[[i,i]].explore(m=map, style_kwds={'weight':3, 'color':'yellow', 'opacity':1},
-                                                  name=f'Prediction {i} ({predicted_paths["probability"].iloc[i]*100}% probability)')
-        folium.LayerControl().add_to(map)
-        return map
 
 
         
